0cd9bb283a1bb9874d7066c8b47bdd80f4c08226
Edson Junior <edson_junior91@hotmail.com>
2020-07-27 23:29:46 -0300

    Exercicio Fatorial

---
+++ cursoJavaCompletoUdemy/src/cursoJavaCompletoUdemy/ExercicioFatorial.java
@@ -0,0 +1,25 @@
+package cursoJavaCompletoUdemy;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioFatorial {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n;
+		System.out.println("Digite um numero para descobrir o fatorial");
+		n = sc.nextInt();
+		
+		int fat = 1;
+		for(int i = 1; i <= n; i++) {
+			fat = fat*i;	
+			
+		}
+		System.out.println("Fatorial n = "+ fat);
+		
+	}
+
+}
