f53f4f5047d1c8cba2d1597ecc9d7a0288c4fd0d
Edson Junior <edson_junior91@hotmail.com>
2020-08-24 23:29:50 -0300

    Busca por funcionários com a função

--- exercicioLista/src/application/Program.java
+++ exercicioLista/src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 		
 		List<Employee> list = new ArrayList<>();
 		
-		System.out.print("How many employees will be registered");
+		System.out.print("How many employees will be registered? ");
 		int n = sc.nextInt();
 		
 		for(int i = 0; i < n; i++) {
@@ -32,15 +32,35 @@ public class Program {
 			
 			list.add(emp);
 		}
-		System.out.println("Enter the employee id that will have salary increase : ");
+		System.out.println();
+		System.out.print("Enter the employee id that will have salary increase : ");
 		int idSalary = sc.nextInt();
-		
-		
+		Integer pos = position(list, idSalary);
+		if(pos == null) {
+			System.out.println("This id does not exist!");
+		}else {
+			System.out.print("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			list.get(pos).increaseSalary(percent);
+		}
+		System.out.println();
+		System.out.println("List of employees: ");
+		for(Employee emp : list) {
+			System.out.println(emp);
+		}
 		
 		
 		
 		sc.close();
 
+	}
+	public static Integer position(List <Employee> list, int id) {
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getId() == id) {
+				return i;
+			}
+		}
+		return null;
 	}
 
 }
